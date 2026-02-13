fc51f41accc68a853ae6cfadf42a4ea1bfed4639
ffpwlgbarkhausen-admin <ffpw.lgbarkhausen@gmail.com>
2026-02-13 10:45:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-self.addEventListener('fetch', () => {
-  // Erfüllt die PWA-Anforderung
+const CACHE_NAME = 'fw-pro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './script.js',
+  'https://cdn.tailwindcss.com'
+];
+
+// Installieren & Dateien in den Cache legen
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Cache-First Strategie: Erst im Cache suchen, dann im Netz
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
